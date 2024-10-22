f23383f2d4c065bb401b943bc14eed1cdcac95ae
Rodolfo Andino <aandino186@gmail.com>
2024-10-22 13:02:40 -0600

    Feat(Shopping Cart): Implementation of shopping car

--- components/ShoppingCartComponent.vue
+++ components/ShoppingCartComponent.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="bg-white rounded-lg flex justify-center items-center flex-col">
+    <div class="bg-white rounded-lg flex justify-center items-center flex-col relative">
         <Icon
             name="mdi:shopping-outline"
-            class="text-[#4b5563] text-2xl"
+            class="text-[#4b5563] text-[30px]"
         />
+        <span
+            :class="`size-4 text-xs grid place-content-center bg-red-600 rounded-full absolute bottom-0.5 right-0 text-white  duration-300 ${
+                items.length > 0 ? 'opacity-100' : 'opacity-0'
+            }`"
+        >
+            {{ items.length }}
+        </span>
     </div>
 </template>
 
@@ -13,5 +20,5 @@ import { storeToRefs } from 'pinia'
 
 const itemsStore = useItemsStore()
 
-const { isLoading } = storeToRefs(itemsStore)
+const { items } = storeToRefs(itemsStore)
 </script>
