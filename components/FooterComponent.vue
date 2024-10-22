f9e72a55089fafacf6e0b86b0149cb61b7754de7
Rodolfo Andino <aandino186@gmail.com>
2024-10-22 13:03:02 -0600

    Feat(Footer): Implementation of sellers info

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,11 +1,36 @@
 <template>
-    <footer class="absolute bottom-0 flex flex-col w-full bg-primary-500 items-center px-5 md:px-8 py-5 justify-center">
-        <h1>footer</h1>
+    <footer
+        v-if="!hasTwentyPoints"
+        class="absolute bottom-0 flex justify-around w-full items-center px-5 md:px-8 py-5"
+    >
+        <div
+            v-for="(seller, index) in allSellers"
+            :key="index"
+            class="w-[80px] truncate"
+        >
+            <span class="text-xs">{{ seller.name }}</span>
+            <div class="flex bg-primary-500 shadow-lg rounded-lg p-2 items-center gap-1 x">
+                <Icon
+                    name="material-symbols:account-circle"
+                    class="text-black md:text-2xl shadow-2xl"
+                />
+                <p>{{ seller.points || 0 }} / 20</p>
+            </div>
+        </div>
     </footer>
 </template>
 
 <script setup lang="ts">
-const currentYear = new Date().getFullYear()
+import { useSellersStore } from '~/store/sellers'
+import { storeToRefs } from 'pinia'
+
+const sellersStore = useSellersStore()
+
+const { allSellers } = storeToRefs(sellersStore)
+
+const hasTwentyPoints = computed(() => {
+    return allSellers.value.some((seller) => (seller.points ?? 0) >= 20)
+})
 </script>
 
 <style scoped></style>
